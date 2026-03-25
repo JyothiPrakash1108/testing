403cc3371f47ccdad354a353ea3736bcfd76e28e
Maddhini Jyothi Prakash <128385441+JyothiPrakash1108@users.noreply.github.com>
2026-03-26 01:34:39 +0530

    fixed bug

--- tests/TS_01.spec.ts
+++ tests/TS_01.spec.ts
@@ -1,34 +1,16 @@
 import { test , expect ,BrowserContext, Page, Browser} from '@playwright/test';
-import { url } from 'node:inspector';
 
 const URL = "https://www.99acres.com/search/property/buy/hyderabad?city=269&keyword=hyderabad&preference=S&area_unit=1&res_com=R";
-
-test.only("TC_01 : To verify property card listing functionality", 
-    async({page}) =>{
-        await page.goto(URL);
-
-        var a_locator =  await page.locator("//a[@class='ellipsis']").nth(0);
-        const [newTab] = await Promise.all([
-    page.context().waitForEvent('page'), // Wait for the new tab to open
-    a_locator.click() // Click the link to open the new tab
-]);
-        await page.waitForTimeout(3000);
-    }
-);
-/*
 test("TC_01 : To verify working of selectors", 
     async({page}) =>{
         await page.goto("https://www.99acres.com/search/property/buy/hyderabad?city=269&keyword=hyderabad&preference=S&area_unit=1&res_com=R");
 
-        var outer_div = await page.locator("//div[@class='tupleNew__contentWrap']").first();
-        var inner_div = await outer_div.locator("//div[@class='tupleNew__locationName ellipsis']");
-        var property_name = await inner_div.innerText();
+        var a_locator =  await page.locator("//a[@class='ellipsis']").first();
+        await a_locator.click();
+        var link = await a_locator.getAttribute("href");
+        var property_name = await a_locator.innerText();
+        console.log('link : '+link);
         console.log('property name : '+property_name);
-
-        // retrive and click link 
-        var inner_link_div = await outer_div.locator("//div[@class='tupleNew__tupleHeading']");
-        var link = await inner_link_div.locator('a').getAttribute("href");
-        await inner_link_div.locator('a').click();
         
         const formatted = property_name
                             .toLowerCase()
@@ -36,36 +18,29 @@ test("TC_01 : To verify working of selectors",
         expect(link?.toLowerCase()).toContain(formatted);
         await page.waitForTimeout(5000);
     }
-)*/
+)
 
-test("TC_04 To validate that visited property is marked with seen tag", 
-    async({browser}) =>{
-        const context : BrowserContext = await browser.newContext();
-        const page : Page = await context.newPage();
+test("TC_04 To validate that visited property is marked with seen tag", async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
 
-        await page.goto("https://www.99acres.com/search/property/buy/hyderabad?city=269&keyword=hyderabad&preference=S&area_unit=1&res_com=R");
-        // click on property retrive and click link 
-        var outer_div = await page.locator("//div[@class='tupleNew__contentWrap']").first();
-        var inner_div = await outer_div.locator("//div[@class='tupleNew__locationName ellipsis']");
-        var inner_link_div = await outer_div.locator("//div[@class='tupleNew__tupleHeading']");
-        /*
-        context.on('page' , async cur_page =>{
-            await cur_page.waitForLoadState(),
-            console.log("Title of page " + await cur_page.title());
-        })
-        await inner_link_div.locator('a').click()*/
-        //console.log("clicked on the link");
-        // event listner to handle new page and trigger new page 
-        
-        const [newPage] = await Promise.all([
-            context.waitForEvent("page"),
-            await inner_link_div.locator('a').click()
-        ]);
-        await newPage.waitForLoadState();
-        
-        //var seenTag = await page.locator("//div[@class='ImgItem__tag']//img[@alt='Seen-Tag']").first();
-        var seen_tag = await page.getByAltText("Seen-Tag").first();
-        console.log(await seen_tag.getAttribute("src"));
-        expect(seen_tag).toBeVisible();       
-    }  
-);
+    await page.goto("https://www.99acres.com/search/property/buy/hyderabad?city=269&keyword=hyderabad&preference=S&area_unit=1&res_com=R");
+
+    var a_locator =  await page.locator("//a[@class='ellipsis']").first();
+
+
+    // Properly wait for new tab
+    const [newPage] = await Promise.all([
+        context.waitForEvent("page"),
+        a_locator.click()
+    ]);
+
+    await newPage.waitForLoadState();
+    await newPage.close();
+
+    const seenTag = await page.locator("//a[@class='ellipsis']/ancestor::div[contains(@class,'tuple')]//img[@alt='Seen-Tag']");
+    console.log(seenTag);
+
+    console.log("Seen tag count:", await seenTag.count());
+    await expect(seenTag).toBeVisible();
+});
